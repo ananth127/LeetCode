7f205275142b53ba89719e141a5cf043e9c52552
Ananth S <138275866+ananth127@users.noreply.github.com>
2024-06-24 16:40:52 +0530

    Time: 3 ms (85.46%), Space: 50 MB (23.17%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,30 +1,28 @@
-class Solution {
+ class Solution {
+    int m=0;
+    int n=0;
     public int numIslands(char[][] grid) {
-        int count = 0;
-        int row = grid.length;
-        int col = grid[0].length;
-
-        for(int i = 0; i < row;i++){
-            for(int j = 0; j < col ; j++){
-                if(grid[i][j] == '1'){
-                    count = count + 1;
-                    dfs(grid,i,j,row,col);
+        m=grid.length;
+        n=grid[0].length;
+        int count=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]=='1'){
+                    dfs(grid,i,j);
+                    count++;
                 }
             }
         }
         return count;
     }
-    public static void dfs(char[][] grid,int i , int j,int row,int col){
-      if(i < 0 || i >= row || j < 0 || j >= col || grid[i][j] == '0') return; //check for base state index out of bound,hitting 0 
-      //or water so recursion stops
-     
-     grid[i][j] = '0'; //changing to value of 1 to 0 when recorded
-
-     dfs(grid,i + 1,j,row,col);
-     dfs(grid,i - 1,j,row,col);
-     dfs(grid,i,j+1,row,col);
-     dfs(grid,i,j-1,row,col);
-
+    public void dfs(char[][] grid,int i,int j){
+        if(i<0||i>=m||j<0||j>=n||grid[i][j]!='1') {
+            return;
+        }
+        grid[i][j]='0';
+        dfs(grid,i,j-1);
+        dfs(grid,i-1, j);
+        dfs(grid,i,j+1);
+        dfs(grid,i+1,j); 
     }
-
-    }
+}
