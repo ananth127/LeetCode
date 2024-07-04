4ebceb5f669add5b92b7c049f9889532cab4e2bd
Ananth S <138275866+ananth127@users.noreply.github.com>
2024-07-04 11:01:54 +0530

    Time: 12 ms (8.91%), Space: 44.4 MB (52.65%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean judgeCircle(String s) {
+        int ud=0;
+        char nud='U';
+        char nlr='L';
+        int lr=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='U' && (nud=='U')){
+                ud++;
+                nud='U';
+            }
+            else if(s.charAt(i)=='D' && nud=='U'){
+                ud--;
+            }
+            if(s.charAt(i)=='L' && (nlr=='L')){
+                lr++;
+                nlr='L';
+            }
+            else if(s.charAt(i)=='R' && nlr=='L'){
+                lr--;
+            }
+
+        }
+        if(ud==0 && lr==0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
