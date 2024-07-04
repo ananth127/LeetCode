3c509923a073c889d805efdb50174e28ec1646af
Ananth S <138275866+ananth127@users.noreply.github.com>
2024-07-04 11:03:33 +0530

    Time: 8 ms (31.6%), Space: 44.2 MB (67.74%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.java
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -5,16 +5,14 @@ class Solution {
         char nlr='L';
         int lr=0;
         for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='U' && (nud=='U')){
+            if(s.charAt(i)=='U'){
                 ud++;
-                nud='U';
             }
             else if(s.charAt(i)=='D' && nud=='U'){
                 ud--;
             }
-            else if(s.charAt(i)=='L' && (nlr=='L')){
+            else if(s.charAt(i)=='L' ){
                 lr++;
-                nlr='L';
             }
             else if(s.charAt(i)=='R' && nlr=='L'){
                 lr--;
