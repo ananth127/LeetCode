5b3061a12a32cca76dfc279dbe833aa0c91e64e9
Ananth S <138275866+ananth127@users.noreply.github.com>
2024-07-05 12:22:01 +0530

    Time: 7 ms (5.43%), Space: 41.5 MB (75.04%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -3,12 +3,14 @@ class Solution {
         int n=word.length();
         int sum=0;
         int i=1;
+        int ans=0;
         System.out.println(n);
         if(n<=8){
             return n;
         }
         else if(n>8){
                 i=n/8;
+                ans=((n%8)*(i+1));
                 while(i>0){
                     sum+=i*8;
                     System.out.println(sum);
@@ -16,7 +18,7 @@ class Solution {
                 }
                  
             }
-            return (sum+((n%8)*2));
+            return (sum+ans);
         
     }
     }
