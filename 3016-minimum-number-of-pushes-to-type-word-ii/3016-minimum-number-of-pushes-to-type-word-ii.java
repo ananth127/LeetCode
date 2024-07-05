cb75aa291307f91fd50bcdd0a23a7f39752c9dcd
Ananth S <138275866+ananth127@users.noreply.github.com>
2024-07-05 14:21:08 +0530

    Time: 18 ms (34.24%), Space: 45.6 MB (39.9%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+class Solution {
+    public int minimumPushes(String word) {
+        int ct[] = new int[26];
+        for(char ch: word.toCharArray()){
+            ct[ch-'a']++;
+        }
+        
+		// sort the array in descending order
+        ct = Arrays.stream(ct).boxed()
+            .sorted(Collections.reverseOrder())
+            .mapToInt(Integer::intValue)
+            .toArray();
+        
+        int res = 0;
+        for(int i=0; i<26; i++){
+            int val = (i)/8;
+            int curr = (val+1)*ct[i];
+            res += curr;
+        }
+        return res;
+    }
+}
